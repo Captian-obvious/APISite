46aed6c821fdb7d2417234746a784bee38fe86ad
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-01 11:17:27 -0400

    Update errors.js

--- js/errors.js
+++ js/errors.js
@@ -26,6 +26,15 @@ window.addEventListener('load', function() {
         <p style="color: #980000">click <a style="color: #ff0000" href="/"><em>here</em></a> to return home.</p>
         `
     };
+    if (is403 === true) {
+        document.title = 'Forbidden (403)';
+        main.innerHTML=`
+        <h1 style="color: #980000">Forbidden. (403)</h1>
+        <h4 style="color: #980000">You do not have permission to view this page.</h4>
+        <img src="/images/404.png" width="120px" height="120px" />
+        <p style="color: #980000">click <a style="color: #ff0000" href="/"><em>here</em></a> to return home.</p>
+        `
+    };
     if (is404 === true) {
         document.title = 'Page Not Found (404)';
         main.innerHTML=`
