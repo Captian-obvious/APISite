8232b8e0fa60b3e77c6aee67048bcd42dc54905e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-01 08:55:34 -0400

    Update MODULE.py

--- InsertCloudAPI/MODULE.py
+++ InsertCloudAPI/MODULE.py
@@ -2,6 +2,9 @@ asseturl='https://assetdelivery.roblox.com/v1/asset/?id='
 
 class cloud:
     def loadAsset(id):
-        
+        theasset = asseturl+id
+        if (theasset!=None):
+            return encode(theasset)
+        ##endif
     ##end
 ##}
