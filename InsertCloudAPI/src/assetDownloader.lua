9c834705b8a0ea7f76694354060552f0af4205ce
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-06 12:27:50 -0400

    Update assetDownloader.lua

--- InsertCloudAPI/src/assetDownloader.lua
+++ InsertCloudAPI/src/assetDownloader.lua
@@ -1,4 +1,7 @@
 local downloader = {}
+local rbxfile = require('github.com/robloxapi/rbxfile')
+local bin = require('github.com/robloxapi/rbxfile/bin')
+local json = require('github.com/robloxapi/rbxfile/json')
 
 function downloader:DownloadAsset(id)
     local url = 'https://www.roblox.com/asset/?id='..id
