918b55e1ef004bd804270be9cb5c5b7744898fd6
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-06 08:30:10 -0400

    Update insertcloud_info.js

--- InsertCloudAPI/js/insertcloud_info.js
+++ InsertCloudAPI/js/insertcloud_info.js
@@ -1,2 +1,14 @@
-var primaryImage = document.getElementById('normal')
-var secondaryImage = document.getElementById('bigger')
+var primaryImage = document.getElementById('normal');
+var secondaryImage = document.getElementById('bigger');
+primaryImage.addEventListener('click',function(){
+    if (primaryImage.hidden != true) {
+        primaryImage.hidden = true
+        secondaryImage.hidden = false
+    };
+});
+secondaryImage.addEventListener('click',function(){
+    if (secondaryImage.hidden != true) {
+        secondaryImage.hidden = true
+        primaryImage.hidden = false
+    };
+});
