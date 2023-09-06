ee9b65f677717383af058d0bf36f52cd1eb879de
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-06 11:21:44 -0400

    Update InsertCloud.lua

--- InsertCloudAPI/src/InsertCloud.lua
+++ InsertCloudAPI/src/InsertCloud.lua
@@ -1,1 +1,45 @@
+local https = game:GetService("HttpService")
+local base_url = 'https://api.sdd2.io/InsertCloudAPI/assets/v1/'
+cloud = {}
 
+function cloud:JSON(t,data)
+    if t:lower()=='encode' then
+        return https:JSONEncode(data)
+    elseif t:lower()=='decode'
+        return https:JSONDecode(data)
+    end
+end
+
+function cloud:Decompile(json)
+    if json.StatusCode~='403' and json.StatusCode~='404' and json.StatusCode~='500' then
+        local theAsset = json.Asset
+        if theAsset~={} then
+            if theAsset.ClassName~=nil then
+                local val = Instance.new(theAsset.ClassName)
+            end
+        end
+    end
+end
+
+function cloud:CompileAsset(asset,parent)
+    if asset~=nil then
+        local model = Instance.new('Model',parent)
+        model.Name=''..id
+        asset.Parent = model
+    end
+end
+
+function cloud:LoadAsset(id)
+    local pre_asset = cloud:Decompile()
+    local final_asset = nil
+    if pre_asset~=nil then
+        final_asset = cloud:CompileAsset(pre_asset)
+    else
+        
+    end
+    if final_asset~=nil then
+        return final_asset
+    end
+end
+
+return cloud
