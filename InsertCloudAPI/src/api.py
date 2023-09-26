e58265051a705bf5a2c4aa7d4828cfcbcc2a7645
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-26 08:44:29 -0400

    Update api.py

--- InsertCloudAPI/src/api.py
+++ InsertCloudAPI/src/api.py
@@ -1,1 +1,28 @@
+import requests
+import json
+import luaparser
+from luaparser import ast
 
+class insertserver:
+    def downloadAsset(id):
+        url = 'https://assetdelivery.roblox.com/v1/asset/?id='+str(id)      
+        REQUEST = requests.get(url)
+        if (REQUEST.status_code<400):
+            rawData = REQUEST.content
+            asset = open(str(id)+".rbxm", "x")
+            asset.write(str(rawData))
+            posted = {'Status_Code': REQUEST.status_code}
+            return asset.read()
+        else:
+            print('REQUEST_ERROR: '+str(REQUEST.status_code))
+        ##endif
+    ##end
+    def downloadAudio(id):
+        url = 'https://api.hyra.io/audio/'+str(id)
+        REQUEST = requests.get(url)
+        if (REQUEST.status_code<400):
+            asset = REQUEST.content
+            return asset
+        ##endif
+    ##end
+##end
